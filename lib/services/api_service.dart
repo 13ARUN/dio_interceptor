0501d065c05327568e679f998a3db80f2d19e922
13ARUN <13aruneswar@gmail.com>
2025-03-04 17:57:38 +0530

    Refactor ApiService: Improve Dio initialization and replace print statements with debugPrint

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,19 +1,21 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class ApiService {
-  late Dio _dio;
-
   ApiService() {
     _initDio();
   }
+  late Dio _dio;
 
   void _initDio() {
-    _dio = Dio(BaseOptions(
-      baseUrl: 'https://reqres.in/api/',
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 3),
-    ));
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://reqres.in/api/',
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 3),
+      ),
+    );
 
     _dio.interceptors.addAll([
       PrettyDioLogger(
@@ -24,16 +26,16 @@ class ApiService {
       ),
       InterceptorsWrapper(
         onRequest: (options, handler) {
-          print('Sending request to ${options.path}');
+          debugPrint('Sending request to ${options.path}');
           options.headers['X-Demo'] = 'Interceptor-Test';
           return handler.next(options);
         },
         onResponse: (response, handler) {
-          print('Response received: ${response.statusCode}');
+          debugPrint('Response received: ${response.statusCode}');
           return handler.next(response);
         },
         onError: (DioException error, handler) {
-          print('Error occurred: ${error.message}');
+          debugPrint('Error occurred: ${error.message}');
           return handler.next(error);
         },
       ),
@@ -48,15 +50,12 @@ class ApiService {
       rethrow;
     }
   }
-  
+
   Future<dynamic> createUser(String name, String job) async {
     try {
       final response = await _dio.post(
         'users',
-        data: {
-          'name': name,
-          'job': job,
-        },
+        data: {'name': name, 'job': job},
       );
       return response.data;
     } catch (e) {
